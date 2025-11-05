95467e7eb2e8cb77101d0d0423b87f0dd7f095df
dan.toropov(home) <dan.toropov@gmail.com>
2025-11-05 22:54:05 +0300

    add first route to api

--- api_app/routers/users.py
+++ api_app/routers/users.py
@@ -7,19 +7,18 @@ from starlette import status
 
 from core.config import settings
 from core.db_helper import db_helper
+from data_app.crud.user import get_active_users
 
 log = logging.getLogger(__name__)
 router = APIRouter()
 
 
-# @router.get("", response_model=UserResponse)
-# async def get_user_rt(
-#     tg_user_id: int, session: AsyncSession = Depends(db_helper.session_getter)
-# ):
-#     user = await get_user(tg_user_id, session)
-#     return user
-#
-#
+@router.get("")
+async def get_users(session: AsyncSession = Depends(db_helper.session_getter)):
+    user = await get_active_users(session)
+    return user
+
+
 # @router.post("", status_code=status.HTTP_200_OK, response_model=UserResponse)
 # async def set_user_rt(
 #     tg_user: UserCreateUpdate,
